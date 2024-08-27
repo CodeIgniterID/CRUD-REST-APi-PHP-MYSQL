ace027eeebb41c68f9c453e6f5dc423a5e5d51c1
Andreas <seo.satu1@gmail.com>
2024-08-27 13:12:24 +0700

    Update setup.php

--- setup.php
+++ setup.php
@@ -2,8 +2,8 @@
 ini_set('display_errors', 0); // 1 untuk show error, 0 hidden error
 ini_set('display_startup_errors', 0); // 1 untuk show error, 0 hidden error
 
-define('BASE_URL_API','https://matel.seosatu.com/API/'); // ganti URL REST API anda 
-define('BASE_URL','http://localhost/php-api/'); // gantu url API client
+define('BASE_URL_API','https://URL/API/'); // ganti URL REST API anda 
+define('BASE_URL','http://localhost/'); // gantu url API client
 define('TOKEN_API','@_123'); // sementara token belum didunakan
 
 ?>
